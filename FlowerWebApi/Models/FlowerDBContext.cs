b7dc62e883a2ad2241db5a9e1b2471bfe85b6304
Ермеева Руфина Камильевна <201706@mck-ktits.ru>
2020-10-29 11:14:39 +0300

    Added model's lists in context

--- FlowerWebApi/Models/FlowerDBContext.cs
+++ FlowerWebApi/Models/FlowerDBContext.cs
@@ -3,9 +3,13 @@ using System;
 
 namespace FlowerWebApi.Models
 {
-    public class FlowerDBContext:DbContext,IDisposable
+    public class FlowerDBContext : DbContext, IDisposable
     {
         public DbSet<Store> Stores { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<ShopProduct> ShopProducts { get; set; }
+        public DbSet<StoreProduct> StoreProducts { get; set; }
 
         public FlowerDBContext(DbContextOptions<FlowerDBContext> options)
             : base(options)
