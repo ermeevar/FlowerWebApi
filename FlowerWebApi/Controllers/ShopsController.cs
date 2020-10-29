17414e5d8ae98db1d1d90bb39589ec681b27c1f6
Ермеева Руфина Камильевна <201706@mck-ktits.ru>
2020-10-29 11:36:53 +0300

    Added shops controller

---
+++ FlowerWebApi/Controllers/ShopsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FlowerWebApi.Models;
+
+namespace FlowerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopsController : ControllerBase
+    {
+        private readonly FlowerDBContext database;
+
+        public ShopsController(FlowerDBContext context)
+        {
+            database = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Shop>>> GetShops()
+        {
+            return await database.Shops.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shop>> GetShop(int id)
+        {
+            var shop = await database.Shops.FindAsync(id);
+
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return shop;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShop(int id, Shop shop)
+        {
+            if (id != shop.Id)
+            {
+                return BadRequest();
+            }
+
+            database.Entry(shop).State = EntityState.Modified;
+            await database.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Shop>> PostShop(Shop shop)
+        {
+            database.Shops.Add(shop);
+            await database.SaveChangesAsync();
+
+            return Accepted();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Shop>> DeleteShop(int id)
+        {
+            var shop = await database.Shops.FindAsync(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            database.Shops.Remove(shop);
+            await database.SaveChangesAsync();
+
+            return shop;
+        }
+    }
+}
