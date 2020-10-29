6f54c06742d62c7b10191c4039252dd6c0f9183e
Ермеева Руфина Камильевна <201706@mck-ktits.ru>
2020-10-29 11:01:58 +0300

    Added shop model

---
+++ FlowerWebApi/Models/Shop.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowerWebApi.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public int StoreId { get; set; }
+
+        public virtual Store Store { get; set; }
+    }
+}
