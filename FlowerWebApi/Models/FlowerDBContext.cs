98c81c238dcd61b3fc01ee4fd0b744bae6808fd5
Ермеева Руфина Камильевна <201706@mck-ktits.ru>
2021-02-10 00:36:23 +0300

    Add user-model

--- FlowerWebApi/Models/FlowerDBContext.cs
+++ FlowerWebApi/Models/FlowerDBContext.cs
@@ -5,11 +5,13 @@ namespace FlowerWebApi.Models
 {
     public class FlowerDBContext : DbContext, IDisposable
     {
+        public DbSet<Account> Accounts { get; set; }
         public DbSet<Store> Stores { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Shop> Shops { get; set; }
         public DbSet<ShopProduct> ShopProducts { get; set; }
         public DbSet<StoreProduct> StoreProducts { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public FlowerDBContext(DbContextOptions<FlowerDBContext> options)
             : base(options)
---
+++ FlowerWebApi/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowerWebApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public byte[] Picture { get; set; }
+        public int AccountId { get; set; }
+    }
+}
