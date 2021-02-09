640ccb11e8e8539afcd6e991b3dd5a41889e770e
Ермеева Руфина Камильевна <201706@mck-ktits.ru>
2021-02-10 01:14:30 +0300

    Add order-controller

---
+++ FlowerWebApi/Controllers/OrdersController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FlowerWebApi.Models;
+
+namespace FlowerWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrdersController : Controller
+    {
+        private readonly FlowerDBContext database;
+
+        public OrdersController(FlowerDBContext context)
+        {
+            database = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Order>> GetOrders()
+        {
+            return await database.Orders.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await database.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(int id, Order order)
+        {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            database.Entry(order).State = EntityState.Modified;
+            await database.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostOrder(Order order)
+        {
+            database.Orders.Add(order);
+            await database.SaveChangesAsync();
+
+            return Accepted();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            var order = await database.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            database.Orders.Remove(order);
+            await database.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
