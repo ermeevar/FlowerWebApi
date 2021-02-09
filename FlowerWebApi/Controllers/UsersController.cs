3b77372b4ad027892bfa1a3a662508469c88148d
Ермеева Руфина Камильевна <201706@mck-ktits.ru>
2021-02-10 01:21:34 +0300

    Add user-controller

---
+++ FlowerWebApi/Controllers/UsersController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FlowerWebApi.Models;
+
+namespace FlowerWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        private readonly FlowerDBContext database;
+
+        public UsersController(FlowerDBContext context)
+        {
+            database = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            return await database.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await database.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            database.Entry(user).State = EntityState.Modified;
+            await database.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostUser(User user)
+        {
+            database.Users.Add(user);
+            await database.SaveChangesAsync();
+
+            return Accepted();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            var user = await database.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            database.Users.Remove(user);
+            await database.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
