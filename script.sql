cd2f7086f257185250eb83e1627f69c282409166
Rufina Ermeeva <ermeeva.r@gmail.com>
2021-05-16 18:26:42 +0300

    Fix script

--- script.sql
+++ script.sql
@@ -1,31 +1,31 @@
-CREATE SCHEMA `flower`;
+CREATE DATABASE `flower`;
 USE `flower`;
 
-CREATE TABLE IF NOT EXISTS `flower`.`orderstatuses` (
+CREATE TABLE IF NOT EXISTS `flower`.`OrderStatuses` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS `flower`.`templatecategories` (
+CREATE TABLE IF NOT EXISTS `flower`.`TemplateCategories` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS `flower`.`productcategories` (
+CREATE TABLE IF NOT EXISTS `flower`.`ProductCategories` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS `flower`.`accounts` (
+CREATE TABLE IF NOT EXISTS `flower`.`Accounts` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `login` VARCHAR(100) NOT NULL,
   `passwordHash` VARCHAR(100) NULL DEFAULT NULL,
   `salt` VARCHAR(100) NULL DEFAULT NULL,
   `role` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`));
-CREATE UNIQUE INDEX `login` ON `flower`.`accounts` (`login` ASC) VISIBLE;
+CREATE UNIQUE INDEX `login` ON `flower`.`Accounts` (`login` ASC) VISIBLE;
 
-CREATE TABLE IF NOT EXISTS `flower`.`stores` (
+CREATE TABLE IF NOT EXISTS `flower`.`Stores` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `description` LONGTEXT NULL DEFAULT NULL,
@@ -34,18 +34,18 @@ CREATE TABLE IF NOT EXISTS `flower`.`stores` (
   `picture` LONGBLOB NULL DEFAULT NULL,
   `accountId` INT NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (accountId) REFERENCES accounts(id) ON DELETE CASCADE);
+  FOREIGN KEY (accountId) REFERENCES Accounts(id) ON DELETE CASCADE);
   
-  CREATE TABLE IF NOT EXISTS `flower`.`shops` (
+  CREATE TABLE IF NOT EXISTS `flower`.`Shops` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `address` VARCHAR(1000) NOT NULL,
   `storeId` INT NOT NULL,
   `accountId` INT NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (accountId) REFERENCES accounts(id) on delete cascade,
-  FOREIGN KEY (storeId) REFERENCES stores(id) on delete cascade);
+  FOREIGN KEY (accountId) REFERENCES Accounts(id) on delete cascade,
+  FOREIGN KEY (storeId) REFERENCES Stores(id) on delete cascade);
 
-CREATE TABLE IF NOT EXISTS `flower`.`products` (
+CREATE TABLE IF NOT EXISTS `flower`.`Products` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `picture` LONGBLOB NULL DEFAULT NULL,
@@ -53,10 +53,10 @@ CREATE TABLE IF NOT EXISTS `flower`.`products` (
   `storeId` INT NOT NULL,
   `productCategoryId` INT NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (storeId) REFERENCES stores(id) on delete cascade,
-  FOREIGN KEY (productCategoryId) REFERENCES productcategories(id) on delete cascade);
+  FOREIGN KEY (storeId) REFERENCES Stores(id) on delete cascade,
+  FOREIGN KEY (productCategoryId) REFERENCES ProductCategories(id) on delete cascade);
 
-CREATE TABLE IF NOT EXISTS `flower`.`users` (
+CREATE TABLE IF NOT EXISTS `flower`.`Users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `surname` VARCHAR(100) NULL DEFAULT NULL,
   `name` VARCHAR(100) NOT NULL,
@@ -64,36 +64,36 @@ CREATE TABLE IF NOT EXISTS `flower`.`users` (
   `picture` LONGBLOB NULL DEFAULT NULL,
   `accountId` INT NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (accountId) REFERENCES accounts(id) on delete cascade);
+  FOREIGN KEY (accountId) REFERENCES Accounts(id) on delete cascade);
 
-CREATE TABLE IF NOT EXISTS `flower`.`bouquets` (
+CREATE TABLE IF NOT EXISTS `flower`.`Bouquets` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NULL DEFAULT NULL,
   `cost` DOUBLE NOT NULL,
   `storeId` INT NULL DEFAULT NULL,
   `userId` INT NOT NULL,
    PRIMARY KEY (`id`),
-   FOREIGN KEY (storeId) REFERENCES stores(id) on delete set null,
-   FOREIGN KEY (userId) REFERENCES users(id) on delete cascade);
+   FOREIGN KEY (storeId) REFERENCES Stores(id) on delete set null,
+   FOREIGN KEY (userId) REFERENCES Users(id) on delete cascade);
 
-CREATE TABLE IF NOT EXISTS `flower`.`bouquetproducts` (
+CREATE TABLE IF NOT EXISTS `flower`.`BouquetProducts` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `productId` INT NOT NULL,
   `bouquetId` INT NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY(productId) REFERENCES products(id) on delete cascade,
-  FOREIGN KEY (bouquetId) REFERENCES bouquets(id) on delete cascade);
+  FOREIGN KEY(productId) REFERENCES Products(id) on delete cascade,
+  FOREIGN KEY (bouquetId) REFERENCES Bouquets(id) on delete cascade);
 
-CREATE TABLE IF NOT EXISTS `flower`.`templates` (
+CREATE TABLE IF NOT EXISTS `flower`.`Templates` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `templateCategoryId` INT NOT NULL,
   `storeId` INT NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (storeId) REFERENCES stores(id) on delete cascade,
-  FOREIGN KEY (templateCategoryId) REFERENCES templateCategories(id) on delete cascade);
+  FOREIGN KEY (storeId) REFERENCES Stores(id) on delete cascade,
+  FOREIGN KEY (templateCategoryId) REFERENCES TemplateCategories(id) on delete cascade);
 
-CREATE TABLE IF NOT EXISTS `flower`.`orders` (
+CREATE TABLE IF NOT EXISTS `flower`.`Orders` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `start` DATETIME NOT NULL,
   `finish` DATETIME NOT NULL,
@@ -106,55 +106,54 @@ CREATE TABLE IF NOT EXISTS `flower`.`orders` (
   `shopId` INT NULL DEFAULT NULL,
   `card` LONGTEXT NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (shopId) REFERENCES shops(id) on delete set null,
-  FOREIGN KEY (userId) REFERENCES users(id) on delete cascade,
-  FOREIGN KEY (orderStatusId) REFERENCES orderStatuses(id) on delete set null,
-  FOREIGN KEY (bouquetId) REFERENCES bouquets(id) on delete set null,
-  FOREIGN KEY (templateId) REFERENCES templates(id) on delete set null);
+  FOREIGN KEY (shopId) REFERENCES Shops(id) on delete set null,
+  FOREIGN KEY (userId) REFERENCES Users(id) on delete cascade,
+  FOREIGN KEY (orderStatusId) REFERENCES OrderStatuses(id) on delete set null,
+  FOREIGN KEY (bouquetId) REFERENCES Bouquets(id) on delete set null,
+  FOREIGN KEY (templateId) REFERENCES Templates(id) on delete set null);
 
-INSERT INTO `flower`.`accounts` (`login`, `passwordHash`, `role`) VALUES ('buket_stolici', 'qwerty', 'store');
-INSERT INTO `flower`.`accounts` (`login`, `passwordHash`, `role`) VALUES ('magazin_21', 'qwerty', 'shop');
-INSERT INTO `flower`.`accounts` (`login`, `passwordHash`, `role`) VALUES ('zavoyskaya_2105', 'qwerty', 'user');
+INSERT INTO `flower`.`Accounts` (`login`, `passwordHash`, `role`) VALUES ('buket_stolici', 'qwerty', 'store');
+INSERT INTO `flower`.`Accounts` (`login`, `passwordHash`, `role`) VALUES ('magazin_21', 'qwerty', 'shop');
+INSERT INTO `flower`.`Accounts` (`login`, `passwordHash`, `role`) VALUES ('zavoyskaya_2105', 'qwerty', 'user');
 
-INSERT INTO `flower`.`users` (`surname`, `name`, `phone`, `accountId`) VALUES ('Завойская', 'Рината', '8(927)022-08-55', '3');
-INSERT INTO `flower`.`users` (`surname`, `name`, `phone`, `accountId`) VALUES ('Ермеева', 'Руфина', '8(900)232-55-52', '3');
+INSERT INTO `flower`.`Users` (`surname`, `name`, `phone`, `accountId`) VALUES ('Завойская', 'Рината', '8(927)022-08-55', '3');
+INSERT INTO `flower`.`Users` (`surname`, `name`, `phone`, `accountId`) VALUES ('Ермеева', 'Руфина', '8(900)232-55-52', '3');
 
-INSERT INTO `flower`.`stores` (`name`, `firstPhone`, `secondPhone`, `accountId`) VALUES ('Букет столицы', '8(900)900-00-90', '8(840)700-20-20', '1');
-INSERT INTO `flower`.`stores` (`name`, `firstPhone`, `secondPhone`, `accountId`) VALUES ('ЦветоBAZA', '8(926)705-05-05', '', '1');
+INSERT INTO `flower`.`Stores` (`name`, `firstPhone`, `secondPhone`, `accountId`) VALUES ('Букет столицы', '8(900)900-00-90', '8(840)700-20-20', '1');
+INSERT INTO `flower`.`Stores` (`name`, `firstPhone`, `secondPhone`, `accountId`) VALUES ('ЦветоBAZA', '8(926)705-05-05', '', '1');
 
-INSERT INTO `flower`.`shops` (`address`, `storeId`, `accountId`) VALUES ('г.Казань, Мавлекаева 67Б', '1', '2');
-INSERT INTO `flower`.`shops` (`address`, `storeId`, `accountId`) VALUES ('г.Казань, Солнышко 20', '2', '2');
+INSERT INTO `flower`.`Shops` (`address`, `storeId`, `accountId`) VALUES ('г.Казань, Мавлекаева 67Б', '1', '2');
+INSERT INTO `flower`.`Shops` (`address`, `storeId`, `accountId`) VALUES ('г.Казань, Солнышко 20', '2', '2');
 
-INSERT INTO `flower`.`productcategories` (`name`) VALUES ('Цветок');
-INSERT INTO `flower`.`productcategories` (`name`) VALUES ('Зелень');
-INSERT INTO `flower`.`productcategories` (`name`) VALUES ('Украшение');
+INSERT INTO `flower`.`ProductCategories` (`name`) VALUES ('Цветок');
+INSERT INTO `flower`.`ProductCategories` (`name`) VALUES ('Зелень');
+INSERT INTO `flower`.`ProductCategories` (`name`) VALUES ('Украшение');
 
-INSERT INTO `flower`.`products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Роза', '128.10', '1', '1');
-INSERT INTO `flower`.`products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Лилия', '200.90', '1', '1');
-INSERT INTO `flower`.`products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Пион', '250.50', '1', '1');
-INSERT INTO `flower`.`products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Лаванда', '15.00', '2', '1');
-INSERT INTO `flower`.`products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Лента красная', '23.70', '3', '1');
-INSERT INTO `flower`.`products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Лаванда', '11.50', '2', '2');
+INSERT INTO `flower`.`Products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Роза', '128.10', '1', '1');
+INSERT INTO `flower`.`Products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Лилия', '200.90', '1', '1');
+INSERT INTO `flower`.`Products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Пион', '250.50', '1', '1');
+INSERT INTO `flower`.`Products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Лаванда', '15.00', '2', '1');
+INSERT INTO `flower`.`Products` (`name`, `cost`, `storeId`, `productcategoryId`) VALUES ('Лаванда', '11.50', '2', '2');
 
-INSERT INTO `flower`.`orderstatuses` (`name`) VALUES ('Ожидание');
-INSERT INTO `flower`.`orderstatuses` (`name`) VALUES ('Изготовление');
-INSERT INTO `flower`.`orderstatuses` (`name`) VALUES ('Готово');
-INSERT INTO `flower`.`orderstatuses` (`name`) VALUES ('Выдано');
+INSERT INTO `flower`.`OrderStatuses` (`name`) VALUES ('Ожидание');
+INSERT INTO `flower`.`OrderStatuses` (`name`) VALUES ('Изготовление');
+INSERT INTO `flower`.`OrderStatuses` (`name`) VALUES ('Готово');
+INSERT INTO `flower`.`OrderStatuses` (`name`) VALUES ('Выдано');
 
-INSERT INTO `flower`.`bouquets` (`name`, `cost`, `storeId`, `userId`) VALUES ('Для Руфинки-мальвинки', '500.90', '1', '1');
+INSERT INTO `flower`.`Bouquets` (`name`, `cost`, `storeId`, `userId`) VALUES ('Для Руфинки-мальвинки', '500.90', '1', '1');
 
-INSERT INTO `flower`.`bouquetproducts` (`productId`, `bouquetId`) VALUES ('1', '1');
-INSERT INTO `flower`.`bouquetproducts` (`productId`, `bouquetId`) VALUES ('2', '1');
+INSERT INTO `flower`.`BouquetProducts` (`productId`, `bouquetId`) VALUES ('1', '1');
+INSERT INTO `flower`.`BouquetProducts` (`productId`, `bouquetId`) VALUES ('2', '1');
 
-INSERT INTO `flower`.`templatecategories` (`name`) VALUES ('День рождение');
-INSERT INTO `flower`.`templatecategories` (`name`) VALUES ('Похороны');
-INSERT INTO `flower`.`templatecategories` (`name`) VALUES ('Свадьба');
-INSERT INTO `flower`.`templatecategories` (`name`) VALUES ('8 марта');
+INSERT INTO `flower`.`TemplateCategories` (`name`) VALUES ('День рождение');
+INSERT INTO `flower`.`TemplateCategories` (`name`) VALUES ('Похороны');
+INSERT INTO `flower`.`TemplateCategories` (`name`) VALUES ('Свадьба');
+INSERT INTO `flower`.`TemplateCategories` (`name`) VALUES ('8 марта');
 
-INSERT INTO `flower`.`templates` (`name`, `templateCategoryId`, `storeId`) VALUES ('Для самых любимых', '4', '1');
-INSERT INTO `flower`.`templates` (`name`, `templateCategoryId`, `storeId`) VALUES ('Радость', '1', '1');
+INSERT INTO `flower`.`Templates` (`name`, `templateCategoryId`, `storeId`) VALUES ('Для самых любимых', '4', '1');
+INSERT INTO `flower`.`Templates` (`name`, `templateCategoryId`, `storeId`) VALUES ('Радость', '1', '1');
 
-INSERT INTO `flower`.`orders` (`start`, `finish`, `bouquetId`, `userId` , `shopId`  , `orderStatusId` , `cost` ) VALUES ('19.12.2020', '21.12.2020', '1', '1', '1', '1', 500.9);
+INSERT INTO `flower`.`Orders` (`start`, `finish`, `bouquetId`, `userId` , `shopId`  , `orderStatusId` , `cost` ) VALUES ('19.12.2020', '21.12.2020', '1', '1', '1', '1', 500.9);
 
 
 
